8580a29cf25d5c04040ce74f754c2bbc8670de5e
Yogesh0777 <94049153+Yogesh0777@users.noreply.github.com>
2023-08-29 16:36:14 +0530

    Update resources.tf

--- custom-role/resources.tf
+++ custom-role/resources.tf
@@ -4,17 +4,17 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 resource "azurerm_role_definition" "custom_role" {
-  count       = length(var.roles)
+  count       = length(var.roles_define)
   name        = var.roles[count.index].name
   description = var.roles[count.index].description
-  scope       = var.scope
+  scope       = var.scope_subs
   permissions {
     actions = var.roles[count.index].actions
     not_actions = var.roles[count.index].not_actions 
